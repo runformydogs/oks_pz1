beada21694fc577fe5109f4f659862d5f0b366c8
runformydogs <andreybydancev@gmail.com>
2024-09-18 13:37:58 +0300

    Добавлен класс и 2 метода Методы Sum, Multiple

--- CCalculator.go
+++ CCalculator.go
@@ -2,9 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	x := 1
-	y := 2
+// Версия 0.0.1.1 от 11.09.2024
 
-	fmt.Println(x + y)
+type CCalculator struct {
+	x, y int
+}
+
+func NewCCalculator(x, y int) *CCalculator {
+	return &CCalculator{x: x, y: y}
+}
+
+func (c *CCalculator) Sum() int {
+	return c.x + c.y
+}
+
+func (c *CCalculator) Multiple() int {
+	return c.x * c.y
+}
+
+func main() {
+	myCalc := NewCCalculator(3, 2)
+
+	fmt.Println("Сумма:", myCalc.Sum())
+	fmt.Println("Произведение:", myCalc.Multiple())
 }
