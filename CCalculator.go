18533d0db4262f1577fa139758fe4f10feb3a14e
runformydogs <andreybydancev@gmail.com>
2024-09-18 13:50:06 +0300

    Добавлен метод Dec

--- CCalculator.go
+++ CCalculator.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Версия 0.0.1.1 от 18.09.2024
+// Версия 0.0.1. от 18.09.2024
 
 type CCalculator struct {
 	x, y int
@@ -20,9 +20,17 @@ func (c *CCalculator) Multiple() int {
 	return c.x * c.y
 }
 
+func (c *CCalculator) Dec() int {
+	return c.x - c.y
+}
+
 func main() {
-	myCalc := NewCCalculator(3, 2)
+	x := 3
+	y := 2 // добавил инициализацию переменных и вывод, тк фиксить нечего
+
+	myCalc := NewCCalculator(x, y)
 
 	fmt.Println("Сумма:", myCalc.Sum())
 	fmt.Println("Произведение:", myCalc.Multiple())
+	fmt.Println("Разность", myCalc.Dec())
 }
